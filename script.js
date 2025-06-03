8bb0e3b4808aa2f7257f3d212207bb84f98449a6
RuslanRuslan89 <ruslankon89@gmail.com>
2025-06-03 11:46:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,14 @@ Email: ${email}
     });
   }
 });
+// === Бургер-меню ===
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav");
+
+  if (menuToggle && nav) {
+    menuToggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
+});
